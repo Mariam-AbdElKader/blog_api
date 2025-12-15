eee0de98ba28dad9f07a42d7557816e47d95dfda
Mariam-AbdElKader <mariamabdelkader62@gmail.com>
2025-12-15 17:27:10 +0200

    Implement authentication and comment management features with Telescope integration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+      protected $fillable = [
+        'title',
+        'content',
+        'img',
+        'is_active',
+    ];
+
+    protected $hidden = [
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
